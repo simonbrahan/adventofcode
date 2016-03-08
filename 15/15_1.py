9544b2ad9b20fe91ac204b145c8e69083995ff22
Simon Brahan <simon.brahan@frogeducation.com>
2016-03-08 13:16:15 +0000

    day 15 solution works

--- 15/15_1.py
+++ 15/15_1.py
@@ -13,6 +13,27 @@ def get_cmd(line):
         'calories': int(res.group(6))
     }
 
+
+def get_recipe_score(ingredients, recipe):
+    capacity = 0
+    durability = 0
+    flavour = 0
+    texture = 0
+    calories = 0
+
+    for label, quantity in recipe.items():
+        capacity += quantity * ingredients[label]['capacity']
+        durability += quantity * ingredients[label]['durability']
+        flavour += quantity * ingredients[label]['flavour']
+        texture += quantity * ingredients[label]['texture']
+        calories += quantity * ingredients[label]['calories']
+
+    return {
+        'score': max(capacity, 0) * max(durability, 0) * max(flavour, 0) * max(texture, 0),
+        'cals': calories
+    }
+
+
 ex_input = open('input.txt', 'r')
 
 ingredients = {}
@@ -20,9 +41,18 @@ for line in ex_input:
     ingredient = get_cmd(line)
     ingredients[ingredient['name']] = ingredient
 
-highest_score = 0
+high_score = 0
 spoonfuls = 100
 for i in range(spoonfuls + 1):
+    recipe = { 'Frosting': i }
     for j in range(spoonfuls - i + 1):
+        recipe['Candy'] = j
         for k in range(spoonfuls - i - j + 1):
-            l = spoonfuls - i - j - k
+            recipe['Butterscotch'] = k
+            recipe['Sugar'] = spoonfuls - i - j - k
+            recipe_stats = get_recipe_score(ingredients, recipe)
+
+            if recipe_stats['cals'] == 500:
+                high_score = max(recipe_stats['score'], high_score)
+
+print high_score
