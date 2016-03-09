cbc420e88e6f24e587743239bebcda9d726013bd
Simon Brahan <simon.brahan@frogeducation.com>
2016-03-09 13:19:22 +0000

    Day 17 part 2

--- 17/17_1.py
+++ 17/17_1.py
@@ -4,10 +4,15 @@ ex_input = open('input.txt')
 containers = [int(size) for size in ex_input]
 
 count = 0
+min_containers = 999
 for container_count in range(1, len(containers) + 1):
     container_options = itertools.combinations(containers, container_count)
     for opts in container_options:
         if sum(opts) is 150:
-            count += 1
+            if len(opts) < min_containers:
+                count = 1
+                min_containers = len(opts)
+            elif len(opts) is min_containers:
+                count += 1
 
 print count
