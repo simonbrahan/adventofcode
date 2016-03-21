5e2b5b61fb5f2e39af1d1913f305f996b5d91685
Simon Brahan <simon.brahan@frogeducation.com>
2016-03-21 14:25:19 +0000

    Day 20 part 1 solution actually works for small numbers - should have been looking for number or greater rather than exact equals

--- 20/20_1.py
+++ 20/20_1.py
@@ -8,9 +8,10 @@ def visit_houses(searched_present_count, num_houses = 1, num_elves = 1):
             if house % elf is 0:
                 houses[house - 1] += elf
 
-    if searched_present_count not in houses:
-        return visit_houses(searched_present_count, num_houses + 1, num_elves + 1)
+    for house, num_presents in enumerate(houses):
+        if num_presents >= searched_present_count:
+            return house + 1;
 
-    return houses.index(searched_present_count) + 1
+    return visit_houses(searched_present_count, num_houses + 1, num_elves + 1)
 
 print visit_houses(searched_present_count)
