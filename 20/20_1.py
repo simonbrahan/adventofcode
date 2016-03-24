b91fabbff826fa1e30a4224db959a404a5092f2b
Simon Brahan <simon.brahan@frogeducation.com>
2016-03-24 13:22:38 +0000

    Day 20 part 1 solultion now works for larger numbers - removed recursion and modulor calculations

--- 20/20_1.py
+++ 20/20_1.py
@@ -1,17 +1,13 @@
-searched_present_count = 13
+searched_present_count = 80
 
-def visit_houses(searched_present_count, num_houses = 1, num_elves = 1):
-    houses = []
-    for house in range(1, num_houses + 1):
-        houses.append(0)
-        for elf in range(1, num_elves + 1):
-            if house % elf is 0:
-                houses[house - 1] += elf
+num_things = 10
 
-    for house, num_presents in enumerate(houses):
-        if num_presents >= searched_present_count:
-            return house + 1;
+houses = [0] * num_things
+for elf in range(1, num_things + 1):
+    for house in range(elf, num_things + 1, elf):
+        house_index = house - 1
+        houses[house_index] += elf * 10
 
-    return visit_houses(searched_present_count, num_houses + 1, num_elves + 1)
-
-print visit_houses(searched_present_count)
+for house_address, num_presents in enumerate(houses):
+    if num_presents >= searched_present_count:
+        print house_address + 1
