f126ddff09b801c79df07753a5cc7c8a7c541c57
Simon Brahan <simon.brahan@frogeducation.com>
2016-04-14 15:24:35 +0100

    Add function comments. Add shield spell and effect. Add boss turn

--- 22/22_1.py
+++ 22/22_1.py
@@ -22,6 +22,7 @@ class State:
     def __init__(self):
         self.boss_health = 0
         self.boss_damage = 0
+
         self.my_health = 0
         self.my_mana = 0
         self.my_armour = 0
@@ -45,6 +46,11 @@ class Node:
         self.spell = spell
         self.state = state
 
+
+'''
+Check that spell can be cast and update mana totals in passed state
+Returns true if spell can be cast, false otherwise
+'''
 def handle_mana(spell, state):
     if state.my_mana < spell.cost:
         return False
@@ -54,30 +60,51 @@ def handle_mana(spell, state):
 
     return True
 
-
+'''
+Deal any damage caused by the passed spell to the boss in the passed state
+'''
 def handle_damage(spell, state):
     state.boss_health -= spell.damage
 
-
+'''
+Handle any healing caused by the passed spell to the player in the passed state
+'''
 def handle_heal(spell, state):
     state.my_health += spell.heal
 
 
+'''
+Update passed state to show activation of passed spell, if it has any effects
+Returns false if an already active spell cannot be cast, true otherwise
+'''
 def handle_effect_activation(spell, state):
-    if state.poison_effect_ticks is 0:
-        state.poison_effect_ticks = spell.poison_effect_ticks
-        state.poison_effect_damage = spell.poison_effect_damage
-    elif spell.name is 'Poison':
-        return False
+    if spell.poison_effect_ticks > 0:
+        if state.poison_effect_ticks is 0:
+            state.poison_effect_ticks = spell.poison_effect_ticks
+            state.poison_effect_damage = spell.poison_effect_damage
+        else:
+            return False
 
-    if state.recharge_effect_ticks is 0:
-        state.recharge_effect_ticks = spell.recharge_effect_ticks
-        state.recharge_effect_mana = spell.recharge_effect_mana
-    elif spell.name is 'Recharge':
-        return False
+    if spell.recharge_effect_ticks > 0:
+        if state.recharge_effect_ticks is 0:
+            state.recharge_effect_ticks = spell.recharge_effect_ticks
+            state.recharge_effect_mana = spell.recharge_effect_mana
+        else:
+            return False
+
+    if spell.shield_effect_ticks > 0:
+        if state.shield_effect_ticks is 0:
+            state.shield_effect_ticks = spell.shield_effect_ticks
+            state.shield_effect_armour = spell.shield_effect_armour
+        else:
+            return False
 
     return True
 
+
+'''
+Carry out any changes to the passed state from active effects
+'''
 def handle_active_effects(state):
     if state.poison_effect_ticks > 0:
         state.boss_health -= spell.poison_effect_damage
@@ -86,6 +113,24 @@ def handle_active_effects(state):
     if state.recharge_effect_ticks > 0:
         state.my_mana += state.recharge_effect_mana
         state.recharge_effect_ticks -= 1
+
+    if state.shield_effect_ticks > 0:
+        state.my_armour = state.shield_effect_armour
+        state.shield_effect_ticks -= 1
+    else:
+        state.my_armour = 0
+
+
+'''
+Deal damage from boss according to passed state
+Return true if player is still alive, false otherwise
+'''
+def handle_boss_damage(state):
+    boss_damage = max(state.boss_damage - state.my_armour, 1)
+
+    state.my_health -= boss_damage
+
+    return state.my_health > 0
 
 
 missile = Spell('Missile')
@@ -97,6 +142,11 @@ drain.cost = 73
 drain.damage = 2
 drain.heal = 2
 
+shield = Spell('Shield')
+shield.cost = 113
+shield.shield_effect_ticks = 6
+shield.shield_effect_armour = 7
+
 poison = Spell('Poison')
 poison.cost = 173
 poison.poison_effect_ticks = 6
@@ -107,10 +157,10 @@ recharge.cost = 229
 recharge.recharge_effect_ticks = 5
 recharge.recharge_effect_mana = 101
 
-spells = [poison, recharge, drain]
+spells = [missile, drain, shield, poison, recharge]
 
 start_state = State()
-start_state.boss_health = 18
+start_state.boss_health = 58
 start_state.boss_damage = 9
 start_state.my_health = 50
 start_state.my_mana = 500
@@ -122,15 +172,17 @@ minimum_spent = None
 while len(queue) > 0:
     node = queue.pop(0)
 
+    # If game is more expensive than current minimum, no need to continue checking
     if not minimum_spent is None and node.state.mana_spent >= minimum_spent:
         continue
 
+    # Effects happen at the start of player's and boss's turns
     handle_active_effects(node.state)
 
+    # If boss is dead following effects, player wins
     if node.state.boss_health <= 0:
         print 'The boss died for ', node.state.mana_spent, 'spent'
         print 'History:', node.state.history
-
 
         if minimum_spent is None or node.state.mana_spent < minimum_spent:
             minimum_spent = node.state.mana_spent
@@ -148,5 +200,25 @@ while len(queue) > 0:
     handle_damage(node.spell, node.state)
     handle_heal(node.spell, node.state)
 
+    # Boss's turn
+
+    # Effects happen at the start of player's and boss's turns
+    handle_active_effects(node.state)
+
+    # If boss is dead following effects, player wins
+    if node.state.boss_health <= 0:
+        print 'The boss died for ', node.state.mana_spent, 'spent'
+        print 'History:', node.state.history
+
+        if minimum_spent is None or node.state.mana_spent < minimum_spent:
+            minimum_spent = node.state.mana_spent
+
+        continue
+
+    if not handle_boss_damage(node.state):
+        continue
+
     for spell in spells:
         queue.append(Node(spell, copy.deepcopy(node.state)))
+
+print 'Cheapest kill:', minimum_spent
