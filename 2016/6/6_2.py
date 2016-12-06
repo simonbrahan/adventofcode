84263220276a0eaf424ad57f17c962c84e3e99f7
Simon Brahan <simon.brahan@frogeducation.com>
2016-12-06 10:54:00 +0000

    Day 6 part 2

---
+++ 2016/6/6_2.py
@@ -0,0 +1,14 @@
+columns = None
+
+for line in open('input', 'r'):
+    if not columns:
+        columns = [ [] for i in range(len(line.strip())) ]
+
+    for idx, char in enumerate(line.strip()):
+        columns[idx].append(char)
+
+output = ''
+for column in columns:
+    output += min(set(column), key=column.count)
+
+print output
