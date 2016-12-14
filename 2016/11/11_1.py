19b15f753f7a1d2d10daa5a5a5ba26a11584f8d8
Simon Brahan <simon.brahan@frogeducation.com>
2016-12-14 13:43:07 +0000

    Day 11 part 1 more wip

--- 2016/11/11_1.py
+++ 2016/11/11_1.py
@@ -1,20 +1,82 @@
-import re, itertools, copy
+import re
 
-def make_move(floor_contents, current_floor, floor_below, move_items):
+def make_move(floor_contents, current_floor, next_floor, move_items):
+    import copy
+    print floor_contents
+    new_floor_contents = copy.deepcopy(floor_contents)
+
+    new_floor_contents[current_floor] = list(set(new_floor_contents[current_floor]) - set(move_items))
+
+    new_floor_contents[next_floor] += move_items
+
+    return new_floor_contents
+
+
+def valid_floor_contents(floor_contents):
+    import re
+
+    for floor in floor_contents:
+        # Only floors with a generator can cause problems
+        if not any([ item for item in floor if ' generator' in item ]):
+            continue
+
+        for item in floor:
+            is_chip = re.match('(\w+)-compatible microchip', item)
+            if not is_chip:
+                continue
+
+            chip_type = is_chip.group(1)
+
+            # floor contains at least one generator, but no generarot for this chip
+            if chip_type + ' generator' not in floor:
+                print 'problem with', floor
+                return False
+
     return True
 
 
 def explore_moves(floor_contents, current_floor, num_moves):
+    import itertools
+
+    if state_explored(floor_contents):
+        return False
+
+    # If first, second and third floor are empty, everything is on floor 4 and we've won
+    if floor_contents[0] == floor_contents[1] == floor_contents[2] == []:
+        print num_moves
+        return True
+
     floor_below = current_floor - 1
     floor_above = current_floor + 1
     possible_moves = itertools.combinations(floor_contents[current_floor] + [None], 2)
 
-    if floor_below in floor_contents.keys():
+    if floor_above < len(floor_contents):
         for move_items in possible_moves:
-            make_move(floor_contents, current_floor, floor_below, move_items)
+            new_floor_contents = make_move(
+                floor_contents,
+                current_floor,
+                floor_above,
+                filter(None, move_items)
+            )
+
+            if valid_floor_contents(new_floor_contents):
+                return explore_moves(new_floor_contents, floor_above, num_moves + 1)
+
+    if floor_below >= 0:
+        for move_items in possible_moves:
+            new_floor_contents = make_move(
+                floor_contents,
+                current_floor,
+                floor_below,
+                filter(None, move_items)
+            )
+
+            if valid_floor_contents(new_floor_contents):
+                return explore_moves(new_floor_contents, floor_below, num_moves + 1)
 
 
 floor_contents = [ [], [], [], [] ]
 for idx, line in enumerate(open('input', 'r')):
     floor_contents[idx] = re.findall('(\w+-compatible microchip)', line) + re.findall('(\w+ generator)', line)
 
+explore_moves(floor_contents, 0, 0)
