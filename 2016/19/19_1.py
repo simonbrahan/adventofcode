f362ed55a7e83c52b85b502e06f8c312998048ef
Simon Brahan <simon.brahan@frogeducation.com>
2016-12-22 09:14:09 +0000

    Day 19 part 1 removed unsed function

--- 2016/19/19_1.py
+++ 2016/19/19_1.py
@@ -1,11 +1,5 @@
 import datetime
 
-def get_next_elf(elf_idx):
-    global elves
-    next_elf_idx = (elf_idx + 1) % len(elves)
-    return next_elf_idx
-
-
 num_elves = 3018458
 
 start_time = datetime.datetime.now()
