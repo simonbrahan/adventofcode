be9a5e604365444e02fbedeec78f4e0099f14151
Simon Brahan <simon.brahan@frogeducation.com>
2017-12-07 13:47:27 +0000

    First go at day 3 part 2

---
+++ 2017/day3/part2.py
@@ -0,0 +1,32 @@
+from sys import argv
+from collections import defaultdict
+from ulamcoords import find_ulam_coords
+
+def sum_adjacent(matrix, coords):
+    return sum([
+        matrix[coords[0] - 1].get(coords[1] - 1, 0),
+        matrix[coords[0] - 1].get(coords[1], 0),
+        matrix[coords[0] - 1].get(coords[1] + 1, 0),
+        matrix[coords[0]].get(coords[1] - 1, 0),
+        matrix[coords[0]].get(coords[1] + 1, 0),
+        matrix[coords[0] + 1].get(coords[1] - 1, 0),
+        matrix[coords[0] + 1].get(coords[1], 0),
+        matrix[coords[0] + 1].get(coords[1] + 1, 0)
+    ])
+
+
+input = int(argv[1])
+
+latest_val = 1
+matrix = defaultdict(dict)
+matrix[0] = {0: 1}
+position_num = 2
+while latest_val < input:
+    coords = find_ulam_coords(position_num)
+    next_val = sum_adjacent(matrix, coords)
+    print 'position', position_num, 'coords', coords, 'next val', next_val
+    matrix[coords[0]][coords[1]] = next_val
+    latest_val = next_val
+    position_num += 1
+
+print latest_val
