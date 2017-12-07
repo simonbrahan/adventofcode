63ba9b7dc30e429e48b757973950de697ad2bab5
simon brahan <tntpiranha@gmail.com>
2017-12-07 21:51:39 +0000

    2017 day 4 part 1 completed in python

---
+++ 2017/day4/part1.py
@@ -0,0 +1,9 @@
+def is_valid_password(password):
+    words = password.split()
+    return len(words) == len(set(words))
+
+
+with open('input.txt') as input_file:
+    output = [line for line in input_file if is_valid_password(line)] 
+
+print len(output)
