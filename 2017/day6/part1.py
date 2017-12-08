d3e343fada279f3408d9319aa4aade27637d1adb
Simon Brahan <simon.brahan@frogeducation.com>
2017-12-08 17:00:58 +0000

    2017 day 6 part 1 completed in python

--- 2017/day6/part1.py
+++ 2017/day6/part1.py
@@ -1,7 +1,19 @@
 with open('input.txt') as input_file:
-    blocks = [int(block) for block in input_file.read().strip().split()]
+    banks = [int(bank) for bank in input_file.read().strip().split()]
 
-print blocks
+memo = set()
+cycles = 0
+while str(banks) not in memo:
+    memo.add(str(banks))
+    high_bank = banks.index(max(banks))
+    high_block_count = banks[high_bank]
+    banks[high_bank] = 0
 
-high_block = blocks.index(max(blocks))
-print high_block
+    for block_num in range(high_block_count):
+        banks[(high_bank + block_num + 1) % len(banks)] += 1
+
+    cycles += 1
+
+print banks
+
+print cycles
