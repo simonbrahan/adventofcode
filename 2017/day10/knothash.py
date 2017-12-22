f5e2294378ddd4f85b06d5b8ef321244356e4488
Simon Brahan <simon.brahan@frogeducation.com>
2017-12-22 09:24:30 +0000

    Moved knothash function to lib to help with day 14

--- 2017/day10/knothash.py
+++ 2017/day10/knothash.py
@@ -5,3 +5,24 @@ def twist(items, start_idx, length):
 
     for idx, items_idx in enumerate(twist_idxs):
         items[items_idx] = twist_vals[idx]
+
+
+def hash(input):
+    from operator import xor
+
+    instrs = [ord(char) for char in input] + [17, 31, 73, 47, 23]
+    sparse_hash = range(256)
+
+    skip_size = 0
+    current_idx = 0
+
+    for i in range(64):
+        for twist_length in instrs:
+            twist(sparse_hash, current_idx, twist_length)
+            current_idx += twist_length + skip_size
+            skip_size += 1
+
+    split_hash = [sparse_hash[i:i+16] for i in range(0, 255, 16)]
+    dense_hash = map(lambda x: reduce(xor, x), split_hash)
+
+    return ''.join(map(lambda num: hex(num)[2:], dense_hash))
--- 2017/day10/part2.py
+++ 2017/day10/part2.py
@@ -1,23 +1,3 @@
-from knothash import twist
-from operator import xor
-
-def hash(input):
-    instrs = [ord(char) for char in input] + [17, 31, 73, 47, 23]
-    sparse_hash = range(256)
-
-    skip_size = 0
-    current_idx = 0
-
-    for i in range(64):
-        for twist_length in instrs:
-            twist(sparse_hash, current_idx, twist_length)
-            current_idx += twist_length + skip_size
-            skip_size += 1
-
-    split_hash = [sparse_hash[i:i+16] for i in range(0, 255, 16)]
-    dense_hash = map(lambda x: reduce(xor, x), split_hash)
-
-    return ''.join(map(lambda num: hex(num)[2:], dense_hash))
-
+from knothash import hash
 
 print hash(open('input.txt').read().strip())
