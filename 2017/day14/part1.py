964113c0d542ad5937a84f5efa80f80b73781d55
Simon Brahan <simon.brahan@frogeducation.com>
2017-12-22 13:31:08 +0000

    Fixed bug in knothash function and cleaned up day 14 part 1

--- 2017/day14/part1.py
+++ 2017/day14/part1.py
@@ -2,15 +2,14 @@ import sys
 sys.path.append('../day10')
 import knothash
 
-def padded_binary(hex_str):
-    return str(bin(int(hex_str, 16)))[2:].rjust(128, '0')
-
+def binary(hex_str):
+    return str(bin(int(hex_str, 16)))[2:]
 
 key = 'oundnydw'
 
 on_count = 0
 for i in range(128):
     hash = knothash.hash(key + '-' + str(i))
-    on_count += len([bit for bit in padded_binary(hash) if bit is '1'])
+    on_count += len([bit for bit in binary(hash) if bit is '1'])
 
 print on_count
