d3469bd3676c470a6b02bf9df4cf0deca68a081d
Simon Brahan <simon.brahan@frogeducation.com>
2018-01-03 09:42:28 +0000

    2017 day 16 part 1 completed in python

---
+++ 2017/day16/part1.py
@@ -0,0 +1,29 @@
+class DanceGroup:
+    def __init__(self, dancers):
+        self.dancers = list(dancers)
+
+    def spin(self, num):
+        self.dancers = self.dancers[-num:] + self.dancers[:-num]
+
+    def exchange(self, a, b):
+        self.dancers[a], self.dancers[b] = self.dancers[b], self.dancers[a]
+
+    def partner(self, a, b):
+        self.exchange(self.dancers.index(a), self.dancers.index(b))
+
+
+dg = DanceGroup('abcdefghijklmnop')
+
+with open('input.txt') as input_file:
+    for instr in input_file.read().strip().split(','):
+        if instr[0] is 's':
+            dg.spin(int(instr[1:]))
+        if instr[0] is 'x':
+            a, b = [int(num) for num in instr[1:].split('/')]
+            dg.exchange(a, b)
+        if instr[0] is 'p':
+            a, b = [actor for actor in instr[1:].split('/')]
+            dg.partner(a, b)
+
+print ''.join(dg.dancers)
+
