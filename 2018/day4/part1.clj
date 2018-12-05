44ecacfd2c3fab9be27339b1bea1c8babc8b6e17
simon brahan <tntpiranha@gmail.com>
2018-12-05 20:08:00 +0000

    2018 day 4 part 1 note parsing in clojure

--- 2018/day4/part1.clj
+++ 2018/day4/part1.clj
@@ -1,9 +1,23 @@
 (def input-lines (line-seq (clojure.java.io/reader "input.in")))
 
+; Get the guard id if present in a note, otherwise nil
+(defn get-guard-id [note-body]
+  (let [guard-id (last (re-find #"Guard #(\d+) begins shift" note-body))]
+    (if guard-id
+      (read-string guard-id)
+      nil)))
+
+; Parse the human readable note
+(defn parse-note [note-body]
+  (let [guard-id (get-guard-id note-body) status {"wakes up" :up "falls asleep" :down}]
+    (if guard-id
+      [guard-id :start]
+      [nil (status note-body)])))
+
 (defn parse-line [input-line]
-  (zipmap
-    [ :date :hour :minute :note ]
-    (rest (re-matches #"\[(\d{4}-\d{2}-\d{2}) (\d{2}):(\d{2})\] ([\w# ]+)" input-line))))
+  (let [[date hour minute note] (rest (re-matches #"\[(\d{4}-\d{2}-\d{2}) (\d{2}):(\d{2})\] ([\w# ]+)" input-line))
+        [guard-id op] (parse-note note)]
+    (zipmap [:date :hour :minute :guard-id :op] [date hour minute guard-id op])))
 
 (defn parse-input [input-lines]
   (map parse-line input-lines))
@@ -14,16 +28,14 @@
 (defn prepare-notes [input-lines]
   (-> input-lines parse-input order-notes))
 
-; Get the guard id if present in a note, otherwise nil
-(defn guard-id [note]
-  (let [guard-id (last (re-find #"Guard #(\d+) begins shift" (note :note)))]
-    (if guard-id
-      (read-string guard-id)
-      nil)))
+; Add guard ids to an ordered list of notes
+(defn add-guard-ids [ordered-notes]
+  (reduce
+    (fn [checked-notes next-note]
+      (if (next-note :guard-id)
+        (conj checked-notes next-note)
+        (conj checked-notes (assoc next-note :guard-id ((last checked-notes) :guard-id)))))
+    []
+    ordered-notes))
 
-; Given an ordered list of notes, return a list of guard ids that applies to each note
-(defn guard-ids [ordered-notes]
-  (let [guard-changes (map guard-id ordered-notes)]
-    (reduce (fn [new val] (if val (conj new val) (conj new (last new)))) [] guard-changes)))
-
-(prn (-> input-lines parse-input order-notes guard-ids))
+(let [notes (-> input-lines prepare-notes add-guard-ids)] (prn notes))
