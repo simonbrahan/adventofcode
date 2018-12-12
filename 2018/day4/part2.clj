599ecb29dcbb46d79071f2f1d86a0f279e0bd8eb
simon brahan <tntpiranha@gmail.com>
2018-12-12 19:56:53 +0000

    2018 day 4 part 2 completed in clojure

---
+++ 2018/day4/part2.clj
@@ -0,0 +1,63 @@
+(def input-lines (line-seq (clojure.java.io/reader "input.in")))
+
+; Get the guard id if present in a note, otherwise nil
+(defn get-guard-id [note-body]
+  (let [guard-id (last (re-find #"Guard #(\d+) begins shift" note-body))]
+    (if guard-id
+      (read-string guard-id)
+      nil)))
+
+; Parse the human readable note
+(defn parse-note [note-body]
+  (let [guard-id (get-guard-id note-body) status {"wakes up" :up "falls asleep" :down}]
+    (if guard-id
+      [guard-id :start]
+      [nil (status note-body)])))
+
+(defn parse-line [input-line]
+  (let [[date hour minute note] (rest (re-matches #"\[(\d{4}-\d{2}-\d{2}) (\d{2}):(\d{2})\] ([\w# ]+)" input-line))
+        [guard-id op] (parse-note note)]
+    (zipmap [:date :hour :minute :guard-id :op] [date (Integer/parseInt hour) (Integer/parseInt minute) guard-id op])))
+
+(defn parse-input [input-lines]
+  (map parse-line input-lines))
+
+(defn order-notes [notes]
+  (sort-by (juxt :date :hour :minute) notes))
+
+(defn prepare-notes [input-lines]
+  (-> input-lines parse-input order-notes))
+
+; Add guard ids to an ordered list of notes
+(defn add-guard-ids [ordered-notes]
+  (reduce
+    (fn [checked-notes next-note]
+      (if (next-note :guard-id)
+        (conj checked-notes next-note)
+        (conj checked-notes (assoc next-note :guard-id ((last checked-notes) :guard-id)))))
+    []
+    ordered-notes))
+
+; Get hash map of minutes spent asleep keyed by guard
+(defn get-all-sleep-minutes [ordered-notes]
+  (let [badconj (fnil conj [])
+        times-by-guard (reduce
+                         (fn [times note]
+                           (assoc times (note :guard-id) (badconj (times (note :guard-id)) (note :minute))))
+                         {}
+                         (remove (fn [note] (= :start (note :op))) ordered-notes))]
+    (into {} (for
+      [[guard-id times] times-by-guard]
+      [guard-id (mapcat (fn [startstop] (apply range startstop)) (partition 2 times))]))))
+
+; Given map of minutes asleep keyed by guard, return total times spent asleep
+(defn total-sleep-minutes [sleep-minutes]
+  (into {} (for [[guard-id minutes] (into [] sleep-minutes)] [guard-id (count minutes)])))
+
+(defn all-minute-sleep-frequencies [guard-id minutes-asleep]
+  (map (fn [minute-count] (cons guard-id minute-count)) (into [] (frequencies minutes-asleep))))
+
+(let [minutes-asleep-by-guard (-> input-lines prepare-notes add-guard-ids get-all-sleep-minutes)
+      minute-records (mapcat #(apply all-minute-sleep-frequencies %) (into [] minutes-asleep-by-guard))
+      [guard-id minute _] (apply max-key last minute-records)]
+  (prn (* guard-id minute)))
